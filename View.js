de3f9aff1e7e05705ec4f237829fdd25d8c0b087
Pavel <zabolotnikovpg2@mail.ru>
2022-12-27 14:52:54 +0300

    New file

--- View.js
+++ View.js
@@ -7,11 +7,28 @@ class View {
     constructor() {
     }
     async userName() {
+        
    const vopros = readlineSync.question('Как тебя зовут?');
    console.log('Ваш ответ', vopros)
     }
-
-
+    async getTopics() {
+        fs.readdir(__dirname, (err, files) => {
+            if (err)
+              console.log(err);
+            else {
+              console.log(`./topics`);
+              files.forEach(file => {
+                console.log(file);
+              })
+            }
+          })
+        
+     
+    }
+//     userTema(){
+//     const tema = readlineSync.question('Выбери тему?');
+//     console.log('Введите вашу тему', tema)
+//     }
 }
 
 module.exports = View
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const readlineSync = require('readline-sync') 
+
+readlineSync.question('Нажми Enter')
+
+const vopros = readlineSync.question('Как тебя зовут?');
+   console.log('Ваш ответ', vopros)
+
+
+   async function getTopics() {     
+fs.readdir(`./topics`, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("Темы");
+      files.forEach(file => {
+        console.log(file.slice(0, -4));
+      })
+    }
+  })
+}
+getTopics()
+const tema = readlineSync.question('Выбери тему'); 
+
+
+async function showQuestions() {
+    const questionsList = fs.readFileSync(`./topics/Выдры.txt`, 'utf-8')
+    const questions = questionsList.split('\n\n').map((el) => ({ question: el.split('\n')[0], answer: el.split('\n')[1] }));
+    
+return questions
+}
+showQuestions()
+
+
+
+const rightAnswers = 'Молодец'
+const wrongAnswers = 'Ещё повезет!'
+
