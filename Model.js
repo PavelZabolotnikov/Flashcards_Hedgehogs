d1048634453dc9b92b00a56d23824bc9472672e1
Pavel <zabolotnikovpg2@mail.ru>
2022-12-27 10:14:04 +0300

    Model continue

--- Model.js
+++ Model.js
@@ -8,8 +8,11 @@ class Model {
     return topic;
   }
    async getQuestion(choice) {
-    
-  
+    const file = await fs.readFile(`./topics/${choice}.txt`, 'utf-8');
+    const array = file.split('\n\n').map((el)=> el.split('\n'));
+    const answer = array.map((el)=> ({question: el[0], answers: [el[1],el[2],el[3]], true:el[4]})) 
+    return answer;
+  }
 }
 
 module.exports = Model
