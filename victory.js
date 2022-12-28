7b80d8578c45aca61cc5911ea61024e5894dfe1f
eddy49reg <akhnazarov1990@gmail.com>
2022-12-28 15:05:32 +0300

    create output function

--- victory.js
+++ victory.js
@@ -8,18 +8,47 @@ function getUserName() {
   console.log("Назови свое имя:");
   return readlineSync.question("===>");
 }
+
 const userName = getUserName();
 
 function getNumberOfQweze() {
   const namesOfFiles = fs.readdirSync("./topics");
   let questionList = "";
-  namesOfFiles.map((el) => (questionList += `${el.slice(0, el.length - 4)}\n`));
+  namesOfFiles.map(
+    (el) =>
+      (questionList += `${
+        fs.readFileSync(`./topics/${el}`, "utf-8").split("\n")[0]
+      }\n`)
+  );
   console.clear();
-  console.log("Выберите тему:");
+  console.log(`${userName}, выбери тему:`);
   console.log(questionList);
   return readlineSync.question("===>");
 }
-const quezeNum = getNumberOfQweze();
 
-console.log(userName);
-console.log(quezeNum);
+function quezeReader() {
+  const quezeNum = getNumberOfQweze();
+  const quezeFilesNames = fs.readdirSync("./topics");
+  return fs.readFileSync(`./topics/${quezeFilesNames[quezeNum - 1]}`, "utf-8");
+}
+
+function runner() {
+  console.log(`Приятно познакомиться ${userName}!`);
+  const questionList = quezeReader().split("\n").slice(1);
+  let counter = 0;
+  for (let i = 0; i < questionList.length; i += 2) {
+    // console.clear();
+    console.log(questionList[i]);
+    answer = readlineSync.question("===>");
+    if (answer.toLowerCase() === questionList[i + 1].toLowerCase()) {
+      counter += 10;
+      console.log(`Верно! Твои баллы: ${counter}`);
+    } else console.log(`Неверно! Правильный ответ: ${questionList[i + 1]}`);
+  }
+  console.log("Квиз окончен! Хотите повторить?");
+  const repeatAnswer = readlineSync.question("===>");
+  if (repeatAnswer === "Да") return runner();
+  return "Спасибо за игру!";
+}
+
+console.log(runner());
