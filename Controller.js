14047b0389e8e8b19018863e45cb3a247be25f95
Pavel <zabolotnikovpg2@mail.ru>
2022-12-28 17:44:14 +0300

    start function run

--- Controller.js
+++ Controller.js
@@ -2,16 +2,11 @@ class Controller {
   constructor(model, view, topic) {
     this.model = model
     this.view = view
-    this.score = 0
+  
   }
 
   async run() {
- const rightAnswer = 'Огонь 🔥🔥🔥'
- const wrongAnswer = 'Не отчаиваемся'
- const whoYou = await this.view.userName();
- this.topics = await this.model.readTopics();
- const elTopic = await this.view.chooseTopics(this.topics);
- const quesObj = await this.model.readQuestions(elTopic);
+ 
   } 
 
 
--- index.js
+++ index.js
@@ -10,24 +10,27 @@ const readlineSync = require("readline-sync");
 
 const player = privetstviePlayers()
 
-const vopros = readlineSync.question("Как тебя зовут?");
-console.log("Ваш ответ", vopros);
 
 async function getTopics() {
+  console.clear();
   fs.readdir(`./topics`, (err, files) => {
-    if (err) console.log(err);
+    if (err) console.log();
     else {
-      console.log("Темы");
-      files.forEach((file) => {
-        console.log(file.slice(0, -4));
+      files.forEach((files) => {
+        console.log(files.slice(0, -4));
+        return readlineSync.question(`=> ${player} выберите тему`);
+        
       });
     }
   });
+   
+  
 }
 getTopics();
-const tema = readlineSync.question("Выбери тему");
 
 async function showQuestions() {
+  console.clear();
+  const quezeNum = getTopics();
   const questionsList = fs.readFileSync(`./topics/Выдры.txt`, "utf-8");
   const questions = questionsList
     .split("\n\n")
