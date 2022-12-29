8b8c347f43533fe86f557501b9ecec5904582485
jkantyukova <jkantyukova@gmail.com>
2022-12-30 00:15:16 +0300

    Add Questions and Answers function

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const fs = require('fs');
-const readline = require('readline');
+const rl = require('readline-sync');
+let count = 0;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-/* Name */
-function namequestion(question) {
-  return new Promise((resolve, reject) => {
-    rl.question(question, (name) => {
-      console.log(`Привет, ${name}! Выбери тему:`);
-      resolve(name);
-    });
-  });
-}
-
-/* temi */
+// Темы
 function topic() {
   const topics = fs.readdirSync('./topics', 'utf-8');
   const arr = topics.map((el) =>
@@ -26,73 +12,66 @@ function topic() {
   return top.join(',\n').replace(/,/g, '');
 }
 
-let top = [];
-function topicquestion(question) {
-  return new Promise((resolve, reject) => {
-    rl.question(question, (topicone) => {
-      top.push(topicone);
-      resolve(topicone);
-    });
-  });
-}
+// Начало игры
+console.log('Добро пожаловать в игру!');
+const userName = rl.question('Как тебя зовут? ');
+console.log(`Привет ${userName}! Выбери тему:`);
+const chooseTopic = rl.question(`${topic()}\n ==>`);
 
-let quest = [];
-function qq() {
-  const topicsq = fs.readdirSync('./topics', 'utf-8');
-  const arrq = topicsq.map((el) =>
+// Вопросы
+function getQuestions() {
+  let quest = [];
+  const topics = fs.readdirSync('./topics', 'utf-8');
+  const arr = topics.map((el) =>
     fs.readFileSync(`./topics/${el}`, 'utf-8').split('\n')
   );
-  // console.log(arrq, 'ghghh'); // 4
-  for (let i = 0; i < arrq.length; i += 1) {
-    if (String(top) === arrq[i][0]) {
-      const as = arrq[i];
-      quest.push(as.filter((el, i) => !((i + 1) % 3)));
+  const num = arr.map((el, i) => el[0][0]);
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (chooseTopic === num[i]) {
+      const ask = arr[i];
+      quest.push(ask.filter((el, i) => !((i + 1) % 3)));
       return quest.flat();
     }
   }
 }
-const qqq = qq();
 
-// let quest = [];
-// let answ = [];
-// function qwerty() {
-//   quest.push(qqq.filter((el, i) => !((i + 1) % 3)));
-//   answ.push(qqq.filter((el, i) => !(i % 3)));
-// }
-// qwerty();
+// Ответы
+function getAnswers() {
+  let answ = [];
+  const topics = fs.readdirSync('./topics', 'utf-8');
+  const arr = topics.map((el) =>
+    fs.readFileSync(`./topics/${el}`, 'utf-8').split('\n')
+  );
+  const num = arr.map((el, i) => el[0][0]);
 
-let questions = [qqq];
-// let answq = answ.flat().slice(1);
-
-async function main() {
-  await namequestion('Как тебя зовут? ');
-  await topicquestion(`${topic()} \nТвой ответ:====> `);
-  await top;
-  await qq();
-  await qqq;
-
-  let answers = [];
-  count = 0;
-  // asking questions one by one
-  for (let question of questions) {
-    // wait for the answer
-    let answer = await new Promise((resolve) => rl.question(question, resolve));
-
-    if (answer === 'i') {
-      count += 10;
-      console.log('yes', count);
-    } else {
-      count -= 5;
-      console.log('no', count);
+  for (let i = 0; i < arr.length; i += 1) {
+    if (chooseTopic === num[i]) {
+      const ask = arr[i];
+      answ.push(ask.filter((el, i) => !(i % 3)));
+      return answ.flat().slice(1);
     }
-    console.log(`You said: ${answer}`);
-
-    answers.push(answer);
   }
-
-  // close at the end
-  rl.close();
-
-  console.log(answers);
 }
-main();
+
+function quiz(question, answer) {
+  const userAnswer = rl.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log('\nЭто правильный ответ!');
+    count += 10;
+  } else {
+    console.log('\nОтвет неверный!');
+    count -= 5;
+  }
+  console.log(`Ваш счет: ${count} баллов!`);
+}
+
+const questions = getQuestions();
+const answers = getAnswers();
+
+for (let i = 0; i < questions.length; i += 1) {
+  quiz(`\nВопрос: ${questions[i]}\n`, answers[i]);
+}
+
+console.log(`Игра подошла к концу! Ваш счет: ${count} баллов!`);
